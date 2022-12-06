1c8ef0d3dbcfaf51ba112182933103a810a62fef
maxbass101 <51973799+maxbass101@users.noreply.github.com>
2022-12-06 20:52:01 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,83 @@
+"use strict"
+
+// console.log("Hello World!!!");
+
+// console.log("Учим "); console.log("JS!");
+
+// let myStr = "Учим " + "JS!";
+// console.log(`myStr = ${myStr}`);
+
+// function myString() {
+//    let myStr = "Учим " + "JS!" + " -- my first function.";
+//    console.log(`myStr = ${myStr}`);
+// }
+// myString();
+
+console.log("Я учу JS");
+
+// Вариант 1
+// console.log('Учим JS')
+
+// Вариант 2
+// console.log('JS')
+// console.log('Учим')
+
+// Вариант 3
+// console.log('Учим') console.log('JS')
+
+// Вариант 4
+// console.log('Учим);
+// console.log('JS');
+// ==============================================================================
+
+// let myAge = 18;
+// myAge = 40;
+// console.log(`myAge = ${myAge}`);
+
+// let myStr;
+// function myString() {
+//    myStr = "Учим " + "JS!" + " -- my first function.";
+// }
+// myString();
+// console.log(`myStr = ${myStr}`);
+
+// let colorUserEye;
+
+// let user;
+// let userName;
+
+// userName = "Вася";
+// user = userName;
+// console.log(user);
+
+// let user = "Вася"; let age = 36;
+
+// const BLOCK_SIZE = 14 + 50;
+// console.log(BLOCK_SIZE);
+// ===========================================================================
+
+// let userName;
+// console.log(typeof userName);
+
+// userName = "Фрилансер по жизни";
+// console.log(typeof userName);
+
+// userName = 58;
+// console.log(typeof userName);
+
+// userName = String(userName);
+// console.log(userName);
+// console.log(typeof userName);
+
+// let userAge = 36;
+// let userInfo = `Фрилансер ${userAge}`;
+// console.log(userInfo);
+
+let userHeight = 145 / 0;
+console.log(userHeight);
+
+let userName;
+console.log(typeof userName);
+
+let userSize = "45" / "8";
+console.log(typeof userSize);
